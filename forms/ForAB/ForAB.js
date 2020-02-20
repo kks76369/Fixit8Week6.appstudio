63354e762195c60bec4dd1c25f56d1fec6bdd139
kks76369 <60583217+kks76369@users.noreply.github.com>
2020-02-20 10:22:52 -0600

    Update ForAB.js

--- forms/ForAB/ForAB.js
+++ forms/ForAB/ForAB.js
@@ -1,3 +1,4 @@
 var names = ["Bob", "Janet", "Tom", "Bob", "Randy", "Elizabeth", "Kim", "Nancy"];
-for (var i = names.length - 1; i >= 0; i--) {
+  for (var i = names.length - 1; i >= 0; i--) {
      console.log(names[i])
+}
